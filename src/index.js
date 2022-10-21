361d858ab38f8ec31bdc029b24fb285296b91b6c
Santiago <santynavarro29@hotmail.com>
2022-10-20 20:48:34 -0500

    Inicial

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-import { saludar } from './js/componentes';
 import './styles.css';
+import { Todo, TodoList } from './classes';
+import { crearTodoHtml } from './js/componentes';
+// import { Todo } from './classes/todo.class';
+// import { TodoList } from './classes/todo-list.class';
 
-const nombre = 'Santiago';
 
-saludar( nombre );
+export const todoList = new TodoList();
+
+// Cuando es el unico elemento el que se exporta puede excluirse en la sintaxis 
+// todoList.todos.forEach(todo => crearTodoHtml(todo)); LO MISMO QUE ABAJO
+todoList.todos.forEach(crearTodoHtml);
